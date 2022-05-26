7d0ff9f32c7335f883659f193e479939db6c2ebe
Michael Bashford <mikebashford@gmail.com>
2022-05-25 21:37:21 -0500

    Added fade in when clicking different images, added opacity to selected image

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const current = document.querySelector("#current");
 const images = document.querySelector(".imgs");
 const image = document.querySelectorAll(".imgs img");
 const opacity = 0.6;
+
+image[0].style.opacity = opacity;
+
+images.addEventListener("click", imgClick);
+
+function imgClick(event) {
+  //reset opacity to default
+  image.forEach((image) => (image.style.opacity = 1));
+  //changes current image to source of clicked image
+  current.src = event.target.src;
+
+  //add fade in class to current image
+  current.classList.add("fade-in");
+
+  //remove the fade in class after half a second
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+
+  //change current thumb opacity
+  event.target.style.opacity = opacity;
+}
